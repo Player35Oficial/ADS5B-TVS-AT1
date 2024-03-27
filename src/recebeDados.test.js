3efc536250ab11d7227028c314f5551ab829d9c2
Yuri Santana <yuri.luana2016@gmail.com>
2024-03-27 19:48:24 -0300

    feat: adjust tests for individual examination

--- src/recebeDados.test.js
+++ src/recebeDados.test.js
@@ -1,10 +1,9 @@
+const dadosEstudante = require("./dadosEstudante");
 
-const dadosEstudante = require('./dadosEstudante');
-
-describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+describe("Teste da função recebeDados", () => {
+  test('Verifica se a função processa corretamente o nome "Yuri Guilherme Santana de Sá" e o RA "181439"', () => {
+    const nome = "Yuri Guilherme Santana de Sá";
+    const RA = "181439";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
